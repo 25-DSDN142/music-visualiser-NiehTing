020699dcb578dd526595a7f8ba4e1a3d2df08f3a
NiehTing <ian.nieh1280@googlemail.com>
2025-09-02 12:04:27 +1200

    Added some colour and shapes

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,37 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
-  
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
+  background(12, 23, 36)
+  rectMode(CENTER);
+  angleMode(DEGREES);
+
+  //Moon
+  let rectSize;
+  rectSize = map(vocal, 0, 100, 75, 125);
+  fill(227, 236, 252);
+  rect(320, 150, rectSize, rectSize, 100);
+
+  //Clouds
+  fill(20)
+  ellipse(130, 100, 70, 30)
+
+  //Terrain
+  rect(0, 480, 1280, 350)
+
+  //Lake
+  fill(20, 43, 59)
+  strokeWeight(0)
+  ellipse(320, 480, 850, 350)
  
-// changes 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+  //Stars
+  //quad(200, 225, 210, 215, 220, 225, 210, 235)
+  rectSize = map(drum, 0, 100, 5, 30);
+  rotate(45);
+  fill(201, 193, 77); 
+   for(let i = 1; i <6; i = i +1){
+      let rectStep = i*50;
+      rect(300, rectStep, rectSize, rectSize); 
+      
+ }
+
 }
